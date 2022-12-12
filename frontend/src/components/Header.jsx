99c9c852df235796f6e296243f9f2c463e8bac3a
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2022-12-12 15:00:43 +0200

    to cafe

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { HiMagnifyingGlass } from "react-icons/hi2";
+import { FiMenu } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
+import { IconContext } from "react-icons";
 
 function Header() {
   return (
     <header>
+      <IconContext.Provider value={{ color: "#e8f92e", size: "150px" }}>
+        <FiMenu />
+      </IconContext.Provider>
+
       <input type="text" />
       <HiMagnifyingGlass />
       <Link to="/">
