52bf7711fa687a97e6bd5cc1ca56cb87c419e54e
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2022-12-13 09:47:04 +0200

    to cafe

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,21 @@
-import "./css/login.css"
+import "./css/login.css";
 
 function Login() {
   return (
-    <div className="login-box">
-      <h4>SignIn</h4>
-      <form>
-        <input type="email" name="email" placeholder="Enter your email" />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-        <input
-          type="button"
-          name="button"
-          value="Enter"
-        />
-      </form>
-    </div>
+    <main>
+      <div className="login-box">
+        <h4>SignIn</h4>
+        <form>
+          <input type="email" name="email" placeholder="Enter your email" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+          <input type="button" name="button" value="Enter" />
+        </form>
+      </div>
+    </main>
   );
 }
 
